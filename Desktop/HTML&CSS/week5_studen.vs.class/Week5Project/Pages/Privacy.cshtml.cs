8aa1b6586e75a4f393b9693a5ebf614b4a7df0aa
ipek57 <151134746+ipek57@users.noreply.github.com>
2025-03-28 15:50:48 +0300

    Update Privacy.cshtml.cs

--- Desktop/HTML&CSS/week5_studen.vs.class/Week5Project/Pages/Privacy.cshtml.cs
+++ Desktop/HTML&CSS/week5_studen.vs.class/Week5Project/Pages/Privacy.cshtml.cs
@@ -1,19 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Week5Project.Pages;
-
-public class PrivacyModel : PageModel
+namespace Week5Project.Pages
 {
-    private readonly ILogger<PrivacyModel> _logger;
-
-    public PrivacyModel(ILogger<PrivacyModel> logger)
+    public class PrivacyModel : PageModel
     {
-        _logger = logger;
-    }
-
-    public void OnGet()
-    {
+        public void OnGet()
+        {
+        }
     }
 }
-
